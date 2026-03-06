6d8006083288b972808a80a0a11be49bdc3a376a
Fernando Amorim <151165080+Fernando204@users.noreply.github.com>
2026-03-06 15:04:46 -0300

    register page created
    criação da pagina de registro

---
+++ 1.0 version/backend/readme.md
@@ -0,0 +1,7 @@
+como rodar o projeto:
+
+$env:JAVA_HOME="C:\Users\...\backend\jdk"
+$env:PATH="$env:JAVA_HOME\bin;$env:PATH"
+
+
+.\mvnw.cmd spring-boot:run
--- 1.0 version/backend/src/main/java/com/example/backend/BackendApplication.java
+++ 1.0 version/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -2,12 +2,15 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.backend.services.Logger;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+
+		Logger.info("server iniciado");
 	}
 
 }
