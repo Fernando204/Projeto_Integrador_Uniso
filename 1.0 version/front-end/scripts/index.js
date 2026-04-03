583095814e85d0adf396bd2818488c053d252801
Tutucr77 <tuliopaulino@hotmail.com>
2026-04-03 11:34:00 -0300

    initializeSales(apiConnection)

--- 1.0 version/front-end/scripts/index.js
+++ 1.0 version/front-end/scripts/index.js
@@ -43,7 +43,7 @@ changeButtons.forEach((bt,index) =>{
         case 2:
             bt.addEventListener("click",()=>{
                 changePage("components/vendas/sales.html").then((res)=>{
-                    initializeSales()
+                    initializeSales(apiConnection);
                 });
             })
             break;
