91df406a5534932c405f3c774d8e37dd4bfb4668
matt <112911417+mateus-citelli@users.noreply.github.com>
2026-04-28 08:29:12 -0300

    movimentação de estoque atualizado
    adicionei linhas no enum de movimentação que faz com que você consiga saber se a movimentação é de entrada ou de saída. Além disso, foi feito o modelo de StockMovment

--- 1.0_version/backend/src/main/java/com/example/backend/models/StockMovment.java
+++ 1.0_version/backend/src/main/java/com/example/backend/models/StockMovment.java
@@ -9,5 +9,39 @@ import jakarta.persistence.Id;
 public class StockMovment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; //id da movimentação
+    private Integer quantidade; //quantidade de mercadoria movimentada de um certo tipo
+
+    public String type; //entrada ou saida
+    public LocalDateTime criadoEm; //cria a data da movimentação
+
+    public StockMovment(Integer quantidade, MovementType type, LocalDateTime criadoEm){ 
+        this.id = id;
+        this.quantidade = quantidade;
+        this.type = type;
+        this.criadoEm = criadoEm;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public Integer quantidade {
+        return quantidade;
+    }
+    public String type {
+        return type;
+    }
+    public LocalDateTime criadoEm {
+        return criadoEm
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+    public void setType(MovementType type) {
+        this.type = type;
+    }
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
 }
