6879541257e8e41d4e3a2edc02d9c49cfa2f5c63
mateus-citelli <112911417+mateus-citelli@users.noreply.github.com>
2026-05-05 08:44:43 -0300

    Update DashboardService.java

--- 1.0_version/backend/src/main/java/com/example/backend/services/DashboardService.java
+++ 1.0_version/backend/src/main/java/com/example/backend/services/DashboardService.java
@@ -1,5 +1,6 @@
 package com.example.backend.services;
 
+import com.example.backend.DTOs.DashboardDTO;
 import com.example.backend.enums.MovmentType;
 import com.example.backend.models.Company;
 import com.example.backend.models.Movment;
