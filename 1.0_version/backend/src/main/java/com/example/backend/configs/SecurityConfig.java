0bfd7b0c8070d84ae1bddb67696b050e2f0a61da
Fernando Amorim <fernadotb2@gmail.com>
2026-05-11 18:30:35 -0300

    feature: new sidebar

--- 1.0_version/backend/src/main/java/com/example/backend/configs/SecurityConfig.java
+++ 1.0_version/backend/src/main/java/com/example/backend/configs/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig {
                                 "/images/**",
                                 "/index.html",
                                 "/components/**",
+                                "/login",
+                                "/register",
                                 "/assets/**",
                                 "/dashboard/**",
                                 "/css/**",
