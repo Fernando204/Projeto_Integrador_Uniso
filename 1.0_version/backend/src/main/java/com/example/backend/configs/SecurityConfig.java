0e96108c030ff17e6356cfee864592e3aad1218a
Fernando Amorim <151165080+Fernando204@users.noreply.github.com>
2026-05-17 11:09:31 -0300

    Atualizar o SecurityConfig.java

--- 1.0_version/backend/src/main/java/com/example/backend/configs/SecurityConfig.java
+++ 1.0_version/backend/src/main/java/com/example/backend/configs/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**","/",
-                                "/images/**",
+                                "/images/**","/recover" 
                                 "/index.html",
                                 "/components/**",
                                 "/login",
